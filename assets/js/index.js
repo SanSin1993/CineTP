254dc0388c2b7085a8e0911565f73ae73130493b
SantiagoSingla <128734650+SanSin1993@users.noreply.github.com>
2023-10-25 20:33:52 -0300

    correccion index

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,25 +2,23 @@ document.addEventListener("DOMContentLoaded", async function () {
 
   if (logo) {
     logo.addEventListener("click", () => {
-      window.location.href = "../index.html";
+        window.location.href = "../index.html";
     });
-  }
+}
 
-  const apiKey = '7f194cf23e4e2305fe113aa39e25592f';
-  const baseUrl = 'https://api.themoviedb.org/3/discover/movie';
-  const estrenos = [];
-  const proximosEstrenos = []
-  const startPage = 1;
-  const endPage = 100;
+  const apiKeyEstrenos = '7f194cf23e4e2305fe113aa39e25592f';
+  const apiKeyProximosEstrenos = '7f194cf23e4e2305fe113aa39e25592f';
+
   const currentDate = new Date().toISOString().split('T')[0];
-  const maxElements = 4;
+  const maxElementsEstrenos = 4;
+  const maxElementsProximosEstrenos =5;
 
-  async function fetchDataForPages() {
-    let page = 1;
-    let totalElementsAdded = 0;
+  async function fetchData(apiKey, pageRange, filterCriteria, maxElements, dataHandler) {
+    const results = [];
+    let page = pageRange[0];
 
-    while (page <= 10 && totalElementsAdded < maxElements) {
-      const apiUrl = `https://api.themoviedb.org/3/discover/movie?page=${page}&api_key=${apiKey}&language=es-ES`;
+    while (page <= pageRange[1] && results.length < maxElements) {
+      const apiUrl = `https://api.themoviedb.org/3/discover/movie?page=${page}&api_key=${apiKey}&language=es-US`;
 
       try {
         const response = await fetch(apiUrl);
@@ -28,18 +26,14 @@ document.addEventListener("DOMContentLoaded", async function () {
         if (response.ok) {
           const data = await response.json();
 
-          const filteredDataEstrenos = data.results.filter(movie => (
-            movie.original_language === "en" &&
-            movie.release_date <= currentDate &&
-            movie.release_date >= '2023-09-01' &&
-            movie.vote_average > 8
-          ));
+          const filteredData = data.results.filter(filterCriteria);
 
-          if (filteredDataEstrenos.length > 0) {
-            const elementsToAdd = Math.min(filteredDataEstrenos.length, maxElements - totalElementsAdded);
-            const moviesToAdd = filteredDataEstrenos.slice(0, elementsToAdd);
-            estrenos.push(...moviesToAdd);
-            totalElementsAdded += elementsToAdd;
+          for (const movie of filteredData) {
+            if (results.length < maxElements) {
+              results.push(movie);
+            } else {
+              break;
+            }
           }
 
           page++;
@@ -51,10 +45,10 @@ document.addEventListener("DOMContentLoaded", async function () {
       }
     }
 
-    displayImagesInCarousel();
+    dataHandler(results);
   }
 
-  function displayImagesInCarousel() {
+  function displayImagesInCarousel(estrenos) {
     const carouselItems = document.querySelectorAll("#carouselExampleAutoplaying .carousel-item img");
 
     for (let i = 0; i < estrenos.length; i++) {
@@ -70,5 +64,64 @@ document.addEventListener("DOMContentLoaded", async function () {
     }
   }
 
-  fetchDataForPages();
+  function createMovieElement(movie, category) {
+    const { title, poster_path, overview, release_date } = movie;
+  
+    if (poster_path === null) {
+      // Si poster_path es null, salta este elemento
+      return;
+    }
+  
+    const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+  
+    const movieContainer = document.createElement("div");
+    movieContainer.classList.add("pelicula");
+    movieContainer.innerHTML = `
+      <img src="${IMG_PATH + poster_path}" alt="${title}">
+      <div class="peliculaInfo">
+          <h3>${title}</h3>
+      </div>
+    `;
+  
+    document.querySelector(`#${category}`).appendChild(movieContainer);
+  
+// Agregar un evento click para mostrar la fecha de estreno
+movieContainer.addEventListener("click", () => {
+  // Obtener la fecha en formato "DD-MM-AA"
+  const formattedReleaseDate = formatDate(release_date);
+
+  // Utilizar SweetAlert para mostrar la fecha de estreno
+  Swal.fire({
+    title: `${title} - Fecha de Estreno`,
+    text: `La película se estrena el ${formattedReleaseDate}`,
+    icon: "info",
+  });
+});
+
+// Función para formatear la fecha a "DD-MM-AA"
+function formatDate(date) {
+  const options = { day: "2-digit", month: "2-digit", year: "numeric" };
+  return new Date(date).toLocaleDateString("es-ES", options);
+}
+
+  }
+  
+
+  fetchData(apiKeyEstrenos, [1, 10], (movie) => (
+    movie.original_language === "en" &&
+    movie.release_date <= currentDate &&
+    movie.release_date >= '2023-09-01' &&
+    movie.vote_average > 7
+  ), maxElementsEstrenos, displayImagesInCarousel);
+
+  fetchData(apiKeyProximosEstrenos, [100, 150], (movie) => (
+    movie.original_language === "en" &&
+    movie.release_date >= currentDate &&
+    movie.release_date <= '2023-12-31' &&
+    movie.poster_path !== 'null' 
+  ), maxElementsProximosEstrenos, (results) => {
+    for (const movie of results) {
+      createMovieElement(movie, "proximosEstrenos");
+    }
+  });
 });
