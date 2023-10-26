eb371d027e06c50d0ecf7789938f53706774438d
SantiagoSingla <128734650+SanSin1993@users.noreply.github.com>
2023-10-26 16:24:03 -0300

    Update scriptNosotros.js

--- assets/js/scriptNosotros.js
+++ assets/js/scriptNosotros.js
@@ -1,10 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Obtener elementos del DOM
-    var logo = document.querySelector(".logo");
-  
-    if (logo) {
-        logo.addEventListener("click", () => {
-            window.location.href = "../index.html";
-        });
-    }
+  if (logo) {
+    logo.addEventListener("click", () => {
+        window.location.href = "../index.html";
+    });
+}
   });
